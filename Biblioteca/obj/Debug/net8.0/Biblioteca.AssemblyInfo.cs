33d5770ad27896a72316511c0dd647b9e96ae8e6
Misael <2008paulomisael@gmail.com>
2025-10-05 18:12:45 -0300

    Criação da classe entidade e autor

--- Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2573b7704931d257f8c71a56ad2908c53c7e115")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22fa9dcda06ca5acb98dedf9e98f9c358396240")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
